d31f62670822563bdd19661199ccdf4510f76181
Angel Igareta <angel@igareta.com>
2020-02-06 16:40:35 +0100

    Changed normalization to a more functional approach

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 # Import packages
+import numpy as np
 import pandas as pd
 
 # Read data
@@ -6,19 +7,20 @@ dataset = pd.read_csv('../data/corpus.csv')
 
 
 # Create method to transform column
-def normalize_seer_stage(seer_stage_column):
-    transformed_seer_stage_column = []
-    for seer_stage_index in range(len(seer_stage_column)):
-        seer_stage = seer_stage_column[seer_stage_index]
-        if seer_stage in [4, 5, 7]:
-            transformed_seer_stage_column.append('later_stage')
-        else:
-            transformed_seer_stage_column.append('early_stage')
-
-    return transformed_seer_stage_column
+def normalize_seer_stage(seer_stage):
+    if seer_stage in [4, 5, 7]:
+        return 'later_stage'
+    elif seer_stage in [8, 9]:
+        return 'non-applicable'
+    else:
+        return 'early_stage'
 
 
 # Add new column with normalization
-dataset['Normalized_seer_stage'] = normalize_seer_stage(dataset['seer_stage'])
+dataset['Normalized_seer_stage'] = dataset.seer_stage.apply(lambda x: normalize_seer_stage(x))
+
+# Prepare variables that will be used
+early_stage_dataset = dataset[dataset.Normalized_seer_stage == 'early_stage']
+later_stage_dataset = dataset[dataset.Normalized_seer_stage == 'later_stage']
 
 print(dataset.head(50))
